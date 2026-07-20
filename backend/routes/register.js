0b8b59df66860312a88f94d918999cf7b613626a
alexarva256 <alexarva001@gmail.com>
2026-07-20 11:47:40 +0300

    Few changes.

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cbor = require('cbor'); // <-- Add this
+const cbor = require('cbor');
 const cache = require('../fido-mds/cache');
 const { validateAuthenticatorTrust } = require('../utils/fido-trust');
 
